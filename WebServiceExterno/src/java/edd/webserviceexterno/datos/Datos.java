59206fb8d84fda90ea960049e49241371b784deb
mcalderon7 <marvin93.0@gmail.com>
2015-09-01 23:55:07 -0600

    Se volvió a hacer el servidor que contendrá las estructuras porque se utilizará con Tomcat.

---
+++ WebServiceExterno/src/java/edd/webserviceexterno/datos/Datos.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edd.webserviceexterno.datos;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author Marvin
+ */
+@WebService(serviceName = "Datos")
+public class Datos {
+
+    /**
+     * Web service operation
+     * @param correo
+     * @param password
+     * @return 
+     */
+    @WebMethod(operationName = "CrearAdministrador")
+    public String CrearAdministrador(@WebParam(name = "correo") String correo, @WebParam(name = "password") String password) {
+        //TODO write your implementation code here:
+        
+        String salida = "Podriamos decir que usted esta creando el administrador con correo "+ correo +" y con la contraseña: " + password;
+        
+        return salida;
+    }
+
+    /**
+     * Web service operation
+     * @param id_estacion_clave
+     * @param nombre
+     * @param password
+     * @return 
+     */
+    @WebMethod(operationName = "CrearEstacionClave")
+    public String CrearEstacionClave(@WebParam(name = "id_estacion_clave") int id_estacion_clave, @WebParam(name = "nombre") String nombre, @WebParam(name = "password") String password) {
+        //TODO write your implementation code here:
+        return null;
+    }
+
+    /**
+     * Web service operation
+     * @param id_estacion_general
+     * @param nombre
+     * @param password
+     * @return
+     */
+    @WebMethod(operationName = "CrearEstacionGeneral")
+    public String CrearEstacionGeneral(@WebParam(name = "id_estacion_general") int id_estacion_general, @WebParam(name = "nombre") String nombre, @WebParam(name = "password") String password) {
+        //TODO write your implementation code here:
+        return null;
+    }
+
+    /**
+     * Web service operation
+     * @param nombre
+     * @param apellido
+     * @param clave
+     * @param password
+     * @return
+     */
+    @WebMethod(operationName = "CrearChofer")
+    public String CrearChofer(@WebParam(name = "nombre") String nombre, @WebParam(name = "apellido") String apellido, @WebParam(name = "clave") int clave, @WebParam(name = "password") String password) {
+        //TODO write your implementation code here:
+        return null;
+    }
+
+    /**
+     * Web service operation
+     * @param id_bus
+     * @return
+     */
+    @WebMethod(operationName = "CrearBus")
+    public String CrearBus(@WebParam(name = "id_bus") int id_bus) {
+        //TODO write your implementation code here:
+        return null;
+    }
+
+    /**
+     * Web service operation
+     * @param nombre
+     * @param estaciones
+     * @return
+     */
+    @WebMethod(operationName = "CrearRuta")
+    public String CrearRuta(@WebParam(name = "nombre") String nombre, @WebParam(name = "estaciones") String estaciones) {
+        //TODO write your implementation code here:
+        return null;
+    }
+}
