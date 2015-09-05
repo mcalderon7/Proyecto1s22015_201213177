8490df42c789076cb21607398f0338ad5ac12219
mcalderon7 <marvin93.0@gmail.com>
2015-09-04 19:00:54 -0600

    Se agregaron los metodos de rotaciones para el árbol AVL.

--- WebServiceExterno/src/java/edd/webserviceexterno/datos/ArbolAVL_Admin.java
+++ WebServiceExterno/src/java/edd/webserviceexterno/datos/ArbolAVL_Admin.java
@@ -16,4 +16,92 @@ public class ArbolAVL_Admin {
         return raiz;
     }
     
+    /*Método de rotación Izquierda Izquierda*/
+    private NodoAVL_Admin rotacionII(NodoAVL_Admin n, NodoAVL_Admin n1) {
+        n.ramaIzquierda(n1.subArbolDerecho());
+        n1.ramaDerecha(n);
+        
+        /*Actualización de los factores de equilibrio*/
+        if(n1.fe == -1) {
+            n.fe = 0;
+            n1.fe = 0;
+        }else {
+            n.fe = -1;
+            n1.fe = 1;
+        }
+        
+        return n1;
+    }
+    
+    private NodoAVL_Admin rotacionDD(NodoAVL_Admin n, NodoAVL_Admin n1) {
+        n.ramaDerecha(n1.subArbolIzquierdo());
+        n1.ramaIzquierda(n);
+        
+        /*Actualización de los factores de equilibrio*/
+        if(n1.fe == +1) {
+            n.fe = 0;
+            n1.fe = 0;
+        }else {
+            n.fe = +1;
+            n1.fe = -1;
+        }
+        
+        return n1;
+    }
+    
+    private NodoAVL_Admin rotacionID(NodoAVL_Admin n, NodoAVL_Admin n1) {
+        NodoAVL_Admin n2;
+        
+        n2 = (NodoAVL_Admin) n1.subArbolDerecho();
+        n.ramaIzquierda(n2.subArbolDerecho());
+        n2.ramaDerecha(n);
+        n1.ramaDerecha(n2.subArbolIzquierdo());
+        n2.ramaIzquierda(n1);
+        
+        /*Actualización de los factores de equilibrio*/
+        if(n2.fe == +1) {
+            n1.fe = -1;
+        }else {
+            n1.fe = 0;
+        }
+        
+        if(n2.fe == -1) {
+            n.fe = 1;
+        }else {
+            n.fe = 0;
+        }
+        
+        n2.fe = 0;
+        
+        return n2;
+    }
+    
+    private NodoAVL_Admin rotacionDI(NodoAVL_Admin n, NodoAVL_Admin n1) {
+        NodoAVL_Admin n2;
+        
+        n2 = (NodoAVL_Admin)n1.subArbolIzquierdo();
+        
+        n.ramaDerecha(n2.subArbolIzquierdo());
+        n2.ramaIzquierda(n);
+        n1.ramaIzquierda(n2.subArbolDerecho());
+        n2.ramaDerecha(n1);
+        
+        /*Actualización de los factores de equilibrio*/
+        if(n2.fe == +1) {
+            n.fe = -1;
+        }else {
+            n.fe = 0;
+        }
+        
+        if(n.fe == -1) {
+            n1.fe = 1;
+        }else {
+            n1.fe = 0;
+        }
+        
+        n2.fe = 0;
+        
+        return n2;
+    }
+    
 }
